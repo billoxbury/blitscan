0417b4e6d414fb741d34e024342e75ac25cb4ed9
Bill Oxbury <oxburybill@gmail.com>
2022-04-26 11:14:31 +0100

    Scraper dashboard added

--- run.sh
+++ run.sh
@@ -19,8 +19,8 @@ txfile="data/bing-tx-master.csv"
 dockerpath="webapp"
 
 # custom search - Bing
-#./scrape/custom_search_bing.py $stfile $qfilebing 
-#./scrape/json_to_csv_bing.py $qfilebing $infile $outfile 
+./scrape/custom_search_bing.py $stfile $qfilebing 
+./scrape/json_to_csv_bing.py $qfilebing $infile $outfile 
 
 # directed (bespoke per-journal) search 
 ./scrape/journal_indexes_to_csv.R $outfile
@@ -32,6 +32,9 @@ dockerpath="webapp"
 # processing
 ./process/score_for_topic.py $outfile $blimodelfile
 ./process/find_species.py $outfile $txfile $birdfile title,abstract
+
+# scraper metrics (R markdown)
+R -e "rmarkdown::render('.scrape/scraper_dashboard.Rmd', html_document(toc = TRUE))"
 
 # clean up
 cp $infile $bakfile
